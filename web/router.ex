a685ba96f15810d2d7fe768ba71e7270d3bac011
Shodai Yokoyama <quantumcars@gmail.com>
2015-09-09 21:42:48 +0900

    Define routes for Note

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule WebappExternalProcess.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    resources "/notes", NoteController
   end
 
   # Other scopes may use custom stacks.
